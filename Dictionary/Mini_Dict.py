75af57286d1bd27f3f55d08272df35ea1246bb53
Kapish Pandoh <73287168+KapishPandoh@users.noreply.github.com>
2021-03-30 02:26:58 +0530

    Create Mini_Dict.py

---
+++ Dictionary/Mini_Dict.py
@@ -0,0 +1,12 @@
+dt = {
+          'Aaronical'  : 'of or like a high priest; pontifical.',
+          'Baby book'  : 'an album for mounting pictures and other memorabilia of a baby and for keeping a record of its growth from infancy.',
+          'Cat'        : 'a small domesticated carnivorous mammal with soft fur, a short snout, and retractable claws. It is widely kept as a pet or for catching mice, and many breeds have been developed.' ,
+          'Father'    : 'male person who begets children',
+          'Eagle Pass' : 'a city in S Texas, on the Rio Grande.'
+       }
+
+
+word = input('Enter word to be search : ')
+word = word.capitalize()
+print(dt[word])
